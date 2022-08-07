7ce3377b04bdeed84171e4cf6d7f6e21755d4a82
mbataprecious <mbataprecious9@gmail.com>
2022-08-07 20:43:13 +0100

    correction

--- set_env.sh
+++ set_env.sh
@@ -1,11 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=mypassword
-export POSTGRES_HOST=postgres.cr9bldgsf1j6.us-east-1.rds.amazonaws.com
+export POSTGRES_USERNAME=mydb
+export POSTGRES_PASSWORD=snazzy10
+export POSTGRES_HOST=snazzypostgres.cswzdwee9xm8.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::mybucket1200798
+export AWS_BUCKET=monotomicro-bucket
 export AWS_REGION=us-east-1
-export AWS_PROFILE=default
+export AWS_PROFILE=adminpresh
 export JWT_SECRET=testing
 export URL=http://localhost:8100
